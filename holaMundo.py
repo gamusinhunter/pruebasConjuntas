dba3a4cfedc6c4776e90475265ba3b51f2edcd7e
marcos <magoluaces@gmail.com>
2024-01-15 16:08:22 +0100

    15Ene Marcos

---
+++ holaMundo.py
@@ -0,0 +1,18 @@
+class Perro:
+    # constructor
+    # def __init__(self, nombre):
+    #     self.nombre = nombre
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def habla(self):
+        print(f'{self.nombre} dice: Guau!!Guau!!')
+
+
+# mi_perro = Perro('Chanchito')
+# mi_perro2 = Perro('Rodrigo')
+# print(mi_perro.nombre)
+# print(mi_perro2.nombre)
+mi_perro = Perro('Rodrigo', 2)
+mi_perro.habla()
